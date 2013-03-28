fca53039ea06182c362ba8ddee06e94eb4b72ce7
Emrah Ayanoglu <emrahayanoglu@gmail.com>
2013-03-28 21:57:30 +0200

    updated

--- rabbitmq-sub/subscribe.rb
+++ rabbitmq-sub/subscribe.rb
@@ -5,13 +5,17 @@ require "rubygems"
 require "amqp"
  
 EventMachine.run do
-  connection = AMQP.connect(:host => 'localhost', :port => 5001)
+  connection = AMQP.connect(:host => '127.0.0.1', :port => 5001)
   puts "Connected to AMQP broker. Running #{AMQP::VERSION} version of the gem..."
  
   channel  = AMQP::Channel.new(connection)
-  queue    = channel.queue("amqpgem.examples.helloworld")
+  queue    = channel.queue("amqpgem.examples.helloworld", :auto_delete => true)
+  exchange = channel.direct("")
  
   queue.subscribe do |payload|
     puts "Received a message: #{payload}. Disconnecting..."
+    connection.close { EventMachine.stop }
   end
-end	
+ 
+  exchange.publish "Hello, world!", :routing_key => queue.name
+end
